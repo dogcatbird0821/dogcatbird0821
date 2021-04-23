efb4555d8ba94312ce06aea4296b871f4713547c
Ogata <Ogata@ogatarei-no-MacBook-Pro.local>
2021-04-23 11:07:49 +0900

    avoid pinch in test

--- plan/js/plan.js
+++ plan/js/plan.js
@@ -6,151 +6,35 @@ if ('serviceWorker' in navigator) {
         console.log('ServiceWorker registration failed: ', err);
     });
 }
-//// ピンチイン禁止
-//document.documentElement.addEventListener('touchstart', function (e) {
-//    if (e.touches.length >= 2) { e.preventDefault(); }
-//}, { passive: false });
 
-//// ダブルタップでズーム禁止
-//let t = 0;
-//document.documentElement.addEventListener('touchend', function (e) {
-//    let now = new Date().getTime();
-//    if ((now - t) < 350) {
-//        e.preventDefault();
-//    }
-//    t = now;
-//}, false);
-
+console.log("hi");
 
 /**
- * ログイン処理
- * 
- */
-const Login = function (user = null) {
+* タッチ操作での拡大縮小禁止
+*/
+function no_scaling() {
+  document.addEventListener("touchmove", mobile_no_scroll, { passive: false });
+  console.log('11');
+}
 
-    if (user) {
-        localStorage.setItem('cachedUser', user);
-    }
+/**
+* タッチ操作での拡大縮小禁止解除
+*/
+function return_scaling() {
+  document.removeEventListener('touchmove', mobile_no_scroll, { passive: false });
+  console.log('22');
+}
 
-    $('#login').addClass('d-none');
-    $('#timecard').removeClass('d-none');
-};
-
-/** 
- * ログアウト処理
- * 
- */
-const Logout = function () {
-    localStorage.removeItem('cachedUser');
-
-    $('#login').removeClass('d-none');
-    $('#timecard').addClass('d-none');
-};
-
-/** 
- * 認証処理（未完）
- * 
- */
-const Authentication = function (user, password) {
-
-    const url = 'https://httpbin.org/headers';
-
-    $.ajax({
-        url: url,
-        type: 'GET'
-    }).done((result) => {
-        console.log(result);
-        Login(user);
-    });
-};
-
-/** 
- * ログインページ初期処理
- * 
- */
-const LoginPageInit = function (user, password) {
-
-    const cachedUser = localStorage.getItem('cachedUser');
-
-    if (cachedUser) {
-        Login();
-    }
-    else {
-        Logout();
-    }
-
-    $('#loginBtn').on('click', () => {
-
-        const inputUser = $('#user').val();
-        const inputPassword = $('#password').val();
-
-        if (!inputUser || !inputPassword) {
-            alert('Please input login information.');
-            return;
-        }
-
-        console.log(inputUser + inputPassword);
-
-        Authentication(inputUser, inputPassword);
-    });
-};
-
-/** 
- * タイムカードページ初期処理
- * 
- */
-const TimeCardPageInit = function () {
-
-    $('#checkInBtn').on('click', () => {
-
-        if (!confirm('It will be checked in.')) return;
-
-        $('#isCheckOut').val('0');
-        console.log('isCheckOut:' + $('#isCheckOut').val());
-
-        const url = 'https://httpbin.org/headers';
-
-        $.ajax({
-            url: url,
-            type: 'GET'
-        }).done((result) => {
-            $('#calendar').text(JSON.stringify(result));
-        });
-    });
-
-    $('#checkOutBtn').on('click', () => {
-        if (!confirm('It will be checked out.')) return;
-
-        $('#isCheckOut').val('1');
-        console.log('isCheckOut:' + $('#isCheckOut').val());
-
-        const url = 'https://httpbin.org/headers';
-
-        $.ajax({
-            url: url,
-            type: 'GET'
-        }).done((result) => {
-            $('#calendar').text(JSON.stringify(result));
-        });
-    });
-
-    $('#registerBtn').on('click', () => {
-        console.log('isCheckOut:' + $('#isCheckOut').val());
-
-        const url = 'https://httpbin.org/headers';
-
-        $.ajax({
-            url: url,
-            type: 'GET'
-        }).done((result) => {
-            $('#calendar').text(JSON.stringify(result));
-        });
-    });
-
-    $('#logoutBtn').on('click', () => {
-        Logout();
-    });
-};
+/**
+* 拡大縮小禁止
+*/
+function mobile_no_scroll(event) {
+  // ２本指での操作の場合
+  if (event.touches.length >= 2) {
+      // デフォルトの動作をさせない
+      event.preventDefault();
+  }
+}
 
 /** 
  * OnLoad
@@ -158,7 +42,7 @@ const TimeCardPageInit = function () {
  */
 document.addEventListener('DOMContentLoaded', () => {
 
-    LoginPageInit();
-    TimeCardPageInit();
+    no_scaling();
+    return_scaling();
 
 }, false);
